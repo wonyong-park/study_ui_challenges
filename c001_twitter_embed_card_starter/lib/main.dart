36b90d4ed96cd3d0f3321a598d42fb5b4381356b
wonyong <wanyong0919@naver.com>
2024-04-11 22:18:33 +0900

    Twitter Embed Card UI 생성

--- c001_twitter_embed_card_starter/lib/main.dart
+++ c001_twitter_embed_card_starter/lib/main.dart
@@ -18,10 +18,6 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Padding(
           padding: EdgeInsets.all(16.0),
-          // Use Center as layout has unconstrained width (loose constraints),
-          // together with SizedBox to specify the max width (tight constraints)
-          // See this thread for more info:
-          // https://twitter.com/biz84/status/1445400059894542337
           child: Center(
             child: SizedBox(
               width: 600, // max allowed width
@@ -39,24 +35,160 @@ class TwitterEmbedCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Delete this and implement the desired layout
     return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text(
-          'Use the icons below to build the completed layout',
-          textAlign: TextAlign.center,
+        /// User And Twitter Icon
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage('assets/andrea-avatar.png'),
+                ),
+                SizedBox(width: 8.0),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Andrea Bizztto',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(width: 8.0),
+                        SvgIcon(
+                          asset: SvgAsset.heartBlue,
+                          width: 20,
+                          height: 20,
+                        ),
+                        SizedBox(width: 8.0),
+                        SvgIcon(
+                          asset: SvgAsset.verified,
+                          width: 20,
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          '@biz84 • ',
+                        ),
+                        Text(
+                          'Follow',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SvgIcon(
+              asset: SvgAsset.x,
+            ),
+          ],
         ),
-        const SizedBox(height: 32.0),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: SvgAsset.values
-              .map((asset) => SvgIcon(
-                    asset: asset,
-                    height: 50,
-                  ))
-              .toList(),
+
+        /// Content Text
+        const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 16.0,
+            ),
+            SelectableText(
+              'Did you know?\n\nWhen you call `MediaQuery.of(context)` inside a build method, the widget will rebuild when *any* of the MediaQuery properties change.\n\nBut there\'s a better way that lets you depend only on the properties you care about (and minimize unnecessary rebuilds). 👇',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+          ],
+        ),
+
+        /// Content Image
+        ClipRRect(
+          borderRadius: BorderRadius.circular(16.0),
+          child: Image.asset(
+            'assets/media-query-banner.jpg',
+          ),
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+
+        /// Content Info
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('10:21 AM • Jun 20, 2023'),
+            SvgIcon(
+              asset: SvgAsset.info,
+              width: 20,
+              height: 20,
+            ),
+          ],
+        ),
+        const Divider(),
+
+        /// Content Heart, Reply, Copy link
+        const Row(
+          children: [
+            SvgIcon(asset: SvgAsset.heartRed),
+            SizedBox(width: 8.0),
+            Text(
+              '997',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(width: 16.0),
+            SvgIcon(asset: SvgAsset.comment),
+            SizedBox(width: 8.0),
+            Text(
+              'Reply',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(width: 16.0),
+            SvgIcon(asset: SvgAsset.link),
+            SizedBox(width: 8.0),
+            Text(
+              'Copy link',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+
+        /// Reply Button
+        Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              side: const BorderSide(color: Colors.black, width: 0.5),
+            ),
+            onPressed: () {},
+            child: const Text(
+              'Read 12 replies',
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+          ),
         ),
       ],
     );
