6c7590a3ea09006cba7ab29b532f3eaa7a74f073
wonyong <wanyong0919@naver.com>
2024-04-17 23:14:36 +0900

    UI 구현

--- c002_radial_countdown_starter/lib/main.dart
+++ c002_radial_countdown_starter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 void main() {
   runApp(const MainApp());
@@ -38,13 +39,38 @@ class CountdownAndRestart extends StatefulWidget {
   CountdownAndRestartState createState() => CountdownAndRestartState();
 }
 
-class CountdownAndRestartState extends State<CountdownAndRestart> {
+class CountdownAndRestartState extends State<CountdownAndRestart> with SingleTickerProviderStateMixin {
   static const maxWidth = 300.0;
+  Duration _elapsed = Duration.zero;
+  late final Ticker _ticker;
+  final int totalTime = 10;
+  int remainingTime = 10;
+  double percent = 1.0;
 
   @override
   void initState() {
+    _ticker = createTicker((elapsed) {
+      setState(() {
+        _elapsed = elapsed;
+        percent = (10000 - _elapsed.inMilliseconds) / 10000;
+        remainingTime = totalTime - _elapsed.inSeconds;
+      });
+
+      if (_elapsed.inSeconds >= 10) _ticker.stop();
+    });
+    _ticker.start();
     super.initState();
-    // TODO: Implement
+  }
+
+  _restart() {
+    _ticker.stop();
+    _ticker.start();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
   }
 
   @override
@@ -53,14 +79,32 @@ class CountdownAndRestartState extends State<CountdownAndRestart> {
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        // TODO: Add countdown widget
-        const Text(
-          'Replace this Text widget with the custom countdown UI',
-          textAlign: TextAlign.center,
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            SizedBox(
+              width: maxWidth,
+              height: maxWidth,
+              child: CircularProgressIndicator(
+                strokeWidth: 25,
+                value: percent,
+                valueColor: const AlwaysStoppedAnimation<Color>(Colors.purple),
+                backgroundColor: Colors.purple[300],
+              ),
+            ),
+            Text(
+              remainingTime.toString(),
+              style: const TextStyle(
+                color: Colors.deepPurple,
+                fontWeight: FontWeight.bold,
+                fontSize: 100,
+              ),
+            ),
+          ],
         ),
         const SizedBox(height: 32),
         ElevatedButton(
-          onPressed: () {}, // TODO: Implement
+          onPressed: _restart,
           child: const Text(
             'Restart',
             style: TextStyle(fontSize: 32),
